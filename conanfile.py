3ecb70e7b8e1c0e66737b4d25dd83c52ae3ec449
rodyherrera <contact@rodyherrera.com>
2026-06-08 01:10:23 -0400

    fix: update dependencies in conanfile.py to latest versions

--- conanfile.py
+++ conanfile.py
@@ -9,11 +9,11 @@ class LineReconstructionDXAConan(ConanFile):
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
     requires = (
-        "opendxa/1.0.0",
+        "opendxa/1.0.3",
         "coretoolkit/1.0.0",
-        "structure-identification/1.0.0",
-        "common-neighbor-analysis/1.0.1",
-        "polyhedral-template-matching/1.0.0",
+        "structure-identification/1.0.1",
+        "common-neighbor-analysis/1.0.2",
+        "polyhedral-template-matching/1.0.4",
         "boost/1.88.0",
         "onetbb/2021.12.0",
         "spdlog/1.14.1",
